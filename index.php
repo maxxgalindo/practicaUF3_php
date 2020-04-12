ad5b56d79ec00d041ee8da3b2dd45626e5c1b05b
aguim <aguimera2000@gmail.com>
2020-04-12 13:17:54 +0200

    Logica front-end LOAD

--- index.php
+++ index.php
@@ -15,19 +15,19 @@ if(isset($_GET['error'])){
 ?>
 
 <form method="post" action="action.php">
-    Name: <input type="text" name="name" required/></br>
-    Category: <input type="text" name="category"/></br>
-   Height: <input type="number"name="height"/></br>
-    Weight: <input type="number"name="weight"/></br>
-    <h3>Type/s</h3></br>
-    Water<input type="checkbox" name = "types[]" value="water"/></br>
-    Fire<input type="checkbox" name = "types[]" value="fire"/></br>
-    Grass<input type="checkbox" name = "types[]" value="grass"/></br>
-    Normal<input type="checkbox" name = "types[]" value="normal"/></br>
+    Name: <input type="text" name="name" value="<?php if(isset($_POST['name'])){ echo $_POST['name'];}?>" required/></br>
+    Category: <input type="text" name="category" value="<?php if(isset($_POST['category'])){ echo $_POST['category'];}?>"/></br>
+   Height: <input type="number"name="height" value="<?php if(isset($_POST['height'])){ echo $_POST['height'];}?>"/></br>
+    Weight: <input type="number"name="weight" value="<?php if(isset($_POST['weight'])){ echo $_POST['wight'];}?>"/></br>
+    <h3>Type/s</h3></br> <?php if(isset($_POST['types'])){$types = $_POST['types'];}else{$types=[];}?>
+    Water<input type="checkbox" name = "types[]" value="water" <?php if(in_array('water',$types)){echo "checked";}?>/></br>
+    Fire<input type="checkbox" name = "types[]" value="fire" <?php if(in_array('fire',$types)){echo "checked";}?>/></br>
+    Grass<input type="checkbox" name = "types[]" value="grass" <?php if(in_array('grass',$types)){echo "checked";}?>/></br>
+    Normal<input type="checkbox" name = "types[]" value="normal" <?php if(in_array('normal',$types)){echo "checked";}?>/></br>
     <h3>Evolutions</h3></br>
-    First: <input type="text" name = "evo1"/></br>
-    Second: <input type="text" name = "evo2"/></br>
-    Third: <input type="text" name = "evo3"/></br>
+    First: <input type="text" name = "evo1" value = "<?php if(isset($_POST['evo1'])){echo $_POST['evo1'];}?>"/></br>
+    Second: <input type="text" name = "evo2" value = "<?php if(isset($_POST['evo2'])){echo $_POST['evo2'];}?>"/></br>
+    Third: <input type="text" name = "evo3" value = "<?php if(isset($_POST['evo3'])){echo $_POST['evo3'];}?>"/></br>
     <input type="submit" name = "save" value ="SAVE"/></br>
     <input type="submit" name = "load" value="LOAD"/></br>
 
